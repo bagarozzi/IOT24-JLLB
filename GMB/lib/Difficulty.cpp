657ac424d03083dabc11ef2effd881dd82f85a44
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-10-16 11:48:53 +0200

    Added library for the game's difficulty

---
+++ GMB/lib/Difficulty.cpp
@@ -0,0 +1,27 @@
+#include "Difficulty.h"
+
+float setDifficulty(int potValue) {
+    int difficultyLevel = map(potValue, 0, 1023, 1, 4);
+    int difficultyFactor = 0;
+    String textDifficulty = "";
+    switch (difficultyLevel) {
+        case 1:
+            difficultyFactor = 1.5;
+            textDifficulty = "Extreme";
+            break;
+        case 2:
+            difficultyFactor = 1.2;
+            textDifficulty = "Hard";
+            break;
+        case 3:
+            difficultyFactor = 1.0;
+            textDifficulty = "Medium";
+            break;
+        case 4:
+            difficultyFactor = 0.8;
+            textDifficulty = "Easy";
+            break;
+    }
+    /* CHIAMARE FUNZIONE STAMPA NOSTRA LIBRERIA PASSANDO TEXT DIFFICULTY */
+    return difficultyFactor;
+}
---
+++ GMB/lib/Difficulty.h
@@ -0,0 +1,5 @@
+/*
+ * A library for calculating and showing the game's difficulty set by a potentiometer.
+*/
+
+float setDifficulty(int potValue);
