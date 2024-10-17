fbf322a151d0052d31a5526fe4ad05babe8e3cbc
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-10-17 17:55:53 +0200

    Implemented menu and wait

--- GMB/main/main.ino
+++ GMB/main/main.ino
@@ -13,66 +13,114 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,20,4);
 /* Arduino PIN definitions: */
 #define POT_PIN A0
 
-#define B1_PIN 2
-#define B2_PIN 3
-#define B3_PIN 4
-#define B4_PIN 5
-
-#define L1_PIN 10
-#define L2_PIN 11
-#define L3_PIN 12
-#define L4_PIN 13
-#define RED_PIN 9
-
 #define SDA A4
 #define SCL A5
+
+int BTN_PIN[] = {2, 3, 4, 5};
+int LED_PIN[] = {10, 11, 12, 13, 9};
 
 /* Global variables: */
 float gameDifficulty = 0.8 // Defaults to "easy"
 
+void (*gamePhase)(void) = mainMenuState();
+long elapsedTime = millis();
+long shutdownTime = 0;
+
+bool BTN_PRESSED[] = {false, false, false, false};
+#define BITSIZE 4
+int currentNumber = 0;
+bool currentBinaryNumber[BITSIZE] = {false, false, false, false};
 
 void setup() {
   lcd.init();
   lcd.backlight();
   Timer1.initialize(10000);
   Timer1.attachInterrupt(sleepGame);
+  elapsedTime = millis();
 }
 
 void loop() {
-
+  elapsedTime = millis() - elapsedTime;
+  gamePhase();
 }
 
-void sleepState() {
-  // vai a letto
-}
-
+/*
+ * The main menu of the game. 
+ */
 void mainMenuState() {
-  /* Set the cursor on the third column and first row. */
+  setMainMenuInterrupts();
   lcd.setCursor(5, 1);
   lcd.print("Welcome to");
   lcd.setCursor(0, 3);
   lcd.print("\"Give me the binary\"");
-  Timer1.start()
-  /* Se il gioco è iniziato allora: 
-  Timer1.stop(), 
-  se si torna alla schermata iniziale allora Timer1.restart()*/
+  gamePhase = waitState;
 }
 
-void gameState() {
+void waitState() {
+  shutdownTime += elapsedTime;
+  if(shutdownTime >= 10000) {
+    gamePhase = sleepState;
+  }
+  else if(BTN_PRESSED[0]) {
+    gamePhase = matchState;
+    BTN_PRESSED[0] = false;
+  }
+}
 
+void matchInit() {
+  currentNumber = random(0, 16);
+  for (int i = 0; i < BITSIZE; i++) {
+    currentBinaryNumber[BITSIZE - 1 - i] = (currentNumber & (1 << i)) ? true : false;
+  }
+}
+
+void matchState() {
+  matchInit();
+
+  gamePhase = endGameState;
 }
 
 void endGameState() {
-
+  // Confronta i vettori
+  bool corrispondono = true;
+  for (int i = 0; i < BITSIZE; i++) {
+    if (currentBinaryNumber[i] != BTN_PRESSED[i]) {
+      corrispondono = false;
+      break;
+    }
+  }
+  gamePhase = matchState;
+  
+  gamePhase = mainMenuState;
 }
 
+/*
+ * The state in which the Arduino sleeps until is woken up. 
+ */
+void sleepState() {
+  disableAllInterrupts();
+  for(int i = 0; i < 3; i++) {
+    enableInterrupt(BTN_PIN[i], wakeUpFunction, RISING);
+  }
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  sleep_mode();
+  sleep_disable();
+  gamePhase = mainMenuState;
+}
+
+void startGameInterrupt() {
+  BTN_PRESSED[0] = true;
+}
+
+void wakeUpFunction() {}
 
 /* Interrupts functions: */
 
 void setMainMenuInterrupts() {
   disableAllInterrupts();
-  enableInterrupt(potPin, potInterrupt, CHANGE); // enableInterrupt(uint8_t pinNumber, void (*userFunction)(void), uint8_t mode)
-  enableInterrupt()
+  enableInterrupt(POT_PIN, setGameDifficulty, CHANGE); // enableInterrupt(uint8_t pinNumber, void (*userFunction)(void), uint8_t mode)
+  enableInterrupt(BTN_PIN[0], startGameInterrupt, RISING);
 }
 
 void disableAllInterrupts() {
@@ -82,20 +130,6 @@ void disableAllInterrupts() {
   disableInterrupt(A0);
 }
 
-void potInterrupt() {
-  gameDifficulty = setDifficulty(analogRead(potPin));
+void setGameDifficulty() {
+  gameDifficulty = getDifficulty(analogRead(potPin));
 }
-
-/*
-void difficulty() {
-  int newDifficulty = 1; //map(analogRead(potPin), 0, 1023, 1, 4);
-  switch (difficultyLevel) {
-
-  }
-  lcd.clear();
-  lcd.setCursor(5, 1);
-  lcd.print("Difficulty");
-  lcd.setCursor(10, 3);
-  lcd.print(newDifficulty);
-}
-*/
