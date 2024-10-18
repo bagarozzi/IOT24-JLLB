057b63f6ae5abe343cadc18eb980627059a4a177
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-10-18 17:31:20 +0200

    Added comments

--- GMB/GameUtils.h
+++ GMB/GameUtils.h
@@ -1,15 +1,29 @@
-/*
- * A file for all the function related to the game. 
-*/
-
 #pragma once
 
+/** 
+ * Returns the difficulty of the game given the value of the
+ * potentiometer. 
+ */
 float getDifficulty(int potValue);
 
+/** 
+ * Returns the match time given the difficulty of the match and the current score.
+ */
 long getMatchTime(float difficulty, int score);
 
+/** 
+ * Returns a random number between 0 and 15 (inclusive).
+ */
 int getRandomNumber();
 
+/** 
+ * Converts "number" into a binary number and copies it into "binaryNumber", 
+ * the most significant bit in position "0".
+ */
 void writeBinaryNumber(int number, bool* binaryNumber);
 
+/** 
+ * Returns wether the binary number (represented as arrays of boolean)
+ * are equal or not.
+ */
 bool checkGuess(bool* guessedNumber, bool* trueNumber);
