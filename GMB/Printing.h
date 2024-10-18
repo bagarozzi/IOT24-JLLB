53098f8e586a11bc4d8fff7f621907b9db6e5688
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-10-18 17:19:03 +0200

    Added comments

--- GMB/Printing.h
+++ GMB/Printing.h
@@ -1,9 +1,21 @@
 #include "constants.h"
 
+/** 
+ * Sets up the LCD display.
+ */
 void setupLCD();
 
+/** 
+ * Prints the welcome message to the display.
+ */
 void printMainMenu();
 
+/**
+ * Prints the number to guess to the display.
+ */
 void printMatch(int currentNumber);
 
+/** 
+ * Prints the endgame message with the current score.
+ */
 void printEndgame(int gameScore, String message);
