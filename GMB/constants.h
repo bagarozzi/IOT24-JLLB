cdc2d2c62fa9cfb904f06e2b3c17a681544f70ab
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-10-18 16:47:33 +0200

    Moved LCD definition to constants file

--- GMB/constants.h
+++ GMB/constants.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <LiquidCrystal_I2C.h>
+
 /* Arduino PIN definitions: */
 #define POT_PIN A0
 
@@ -9,7 +11,9 @@
 #define BUTTON_BOUNCING_TIME 100
 
 /* Button and LED PINs: */
-
 const int BTN_PIN[] = {5, 4, 3, 2};
 const int LED_PIN[] = {13, 12, 11, 10};
 const int RED_PIN = 9;
+
+/* The LCD display: */
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,20,4);
