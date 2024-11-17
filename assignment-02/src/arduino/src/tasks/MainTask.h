2b8498dc3193cd01363825de6890928cd4fb4efe
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-11-17 16:36:20 +0100

    Added header file for the main task

---
+++ assignment-02/src/arduino/src/tasks/MainTask.h
@@ -0,0 +1,37 @@
+#ifndef __MAIN_TASK__
+#define __MAIN_TASK__
+
+#include "kernel/Task.h"
+#include "model/SmartWasteBin.h"
+
+class MainTask : public Task {
+
+    public:
+
+        MainTask();
+        void tick();
+
+    private:
+
+        void setState(int state);
+        long elapsedTimeInState();
+        // TODO: LogOnce() method
+
+        enum {
+            WAITING_FOR_USER,
+            SLEEPING,
+            USER_DETECTED,
+            BIN_OPENING,
+            IN_MAINTENANCE,
+            DISPOSING,
+            BIN_CLOSING
+        } state;
+
+        SmartWasteBin* wasteBin;
+
+        long stateTimeStamp;
+        bool justEnteredState;
+        
+};
+
+#endif
