ae163e904dbf14289523a063b579cecf3019896d
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-11-17 18:22:10 +0100

    Implemented all control flows

--- assignment-02/src/arduino/src/tasks/MainTask.cpp
+++ assignment-02/src/arduino/src/tasks/MainTask.cpp
@@ -13,6 +13,9 @@ void MainTask::tick() {
         case WAITING_FOR_USER:
             // logOnce("Waiting for user");
             if(wasteBin->samplePresence()) {
+                // TODO: user console turn on display
+                // TODO: user console welcome message
+                wasteBin->userDetected();
                 setState(USER_DETECTED);
             }
             else if(elapsedTimeInState() > SLEEP_TIMEOUT) {
@@ -20,7 +23,7 @@ void MainTask::tick() {
             }
             break;
         case SLEEPING:
-            // logOnce("Sleeping");
+            // TODO: logOnce("Sleeping");
             wasteBin->prepareForSleep();
             // TODO: user console prepare sleep
             delay(100);
@@ -32,22 +35,49 @@ void MainTask::tick() {
             setState(WAITING_FOR_USER);
             break;
         case USER_DETECTED:
-            // Handle USER_DETECTED state
+            // TODO: logOnce("User detected");
+            if(wasteBin->isReadyToOpen()) {
+                // TODO: user console ready to open
+                setState(BIN_OPENING);
+            }
+            else if(wasteBin->isUserGone()) {
+                // TODO: user console user gone
+                setState(WAITING_FOR_USER);
+            }
             break;
         case BIN_OPENING:
-            // Handle BIN_OPENING state
-            break;
-        case IN_MAINTENANCE:
-            // Handle IN_MAINTENANCE state
+            // TODO: logOnce("Bin opening");
+            if(wasteBin->isBinOpen()) {
+                // TODO: logOnce("Bin open");
+                setState(DISPOSING);
+            }
             break;
         case DISPOSING:
-            // Handle DISPOSING state
+            // logOnce("Disposing...");
+            if(wasteBin->isDisposingDone()) {
+                // logOnce("Disposing done");
+                setState(BIN_CLOSING);
+            }
+            else if (wasteBin->isInMaintenance()) {
+                // logOnce("Problem detected");
+                setState(IN_MAINTENANCE);
+            }
+            break;
+        case IN_MAINTENANCE:
+            if(wasteBin->isMaintenanceCompleted()) {
+                setState(WAITING_FOR_USER);
+            }
             break;
         case BIN_CLOSING:
-            // Handle BIN_CLOSING state
+            // TODO: logOnce("Bin closing");
+            if(wasteBin->isBinOpen()) {
+                // TODO: logOnce("Bin closed");
+                setState(WAITING_FOR_USER);
+            }
             break;
         default:
             // Handle unknown state
+            Serial.println("Unknown state🦶");
             break;
     }
 
