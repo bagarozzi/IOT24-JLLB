d67de914fa9cf50b04c78959af90c8dad254cd48
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-11-20 11:28:18 +0100

    Added constants for temperature check task

--- assignment-02/src/arduino/src/config.h
+++ assignment-02/src/arduino/src/config.h
@@ -10,6 +10,11 @@
 #define LID_OPEN_TIMEOUT 120000
 #define TELEMETRY_TIMEOUT 500
 
+// Temperature check task's timing configuration: 
+#define TEMPERATURE_CHECK_TIMEOUT 500 // Checks every 500ms
+#define TEMPERATURE_CHECK_MAXTIME 5000 // If the temp. goes "MAXTEMP" for 5 seconds
+#define TEMPERATURE_CHECK_MAXTEMP 40 // The maximum temperature allowed
+
 // PINs
 
 #endif
