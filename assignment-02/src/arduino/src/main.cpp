2350ffc74b8dfca0a3fab6cda4d3da938000a100
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-11-21 11:31:42 +0100

    Moved initalization inside setup()

--- assignment-02/src/arduino/src/main.cpp
+++ assignment-02/src/arduino/src/main.cpp
@@ -5,11 +5,19 @@
 
 Scheduler scheduler;
 
-SmartWasteBin* wasteBin = new SmartWasteBin();
-// UserConsole* uCons = new UserConsoler();
+SmartWasteBin* wasteBin;
+// UserConsole* userConsole;
 
 void setup() {
   scheduler.init(BASE_PERIOD);
+  
+  wasteBin = new SmartWasteBin();
+  wasteBin->init();
+
+  // userConsoler = new UserConsole();
+  // userConsole->init();
+
+  /* Initialize and add to the scheduler all the tasks: */
 }
 
 void loop() {
