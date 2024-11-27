6b394164a96cdc10d3b9fb02236043958b201ab4
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-11-27 11:49:09 +0100

    Implemented methods for user detection

--- assignment-02/src/arduino/src/model/SmartWasteBin.cpp
+++ assignment-02/src/arduino/src/model/SmartWasteBin.cpp
@@ -11,3 +11,28 @@ void SmartWasteBin::init() {
 void SmartWasteBin::setState(State s) {
     state = s;
 }
+
+// Methods for user detection
+
+void SmartWasteBin::userDetected() {
+    setState(USER_DETECTED);
+}
+
+bool SmartWasteBin::isUserDetected() {
+    return this->state == USER_DETECTED;
+}
+
+void SmartWasteBin::userGone() {
+    setState(USER_GONE);
+}
+
+bool SmartWasteBin::isUserGone() {
+    return this->state == USER_GONE;
+}
+
+void SmartWasteBin::readyToOpen() {
+    setState(READY_TO_OPEN);
+}
+bool SmartWasteBin::isReadyToOpen() {
+    return this->state == READY_TO_OPEN;
+}
