24a8b2dac30ce4acd23c50eebd0d8efda08de4c1
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-11-29 12:06:38 +0100

    Implementation of MessageService

---
+++ assignment-02/src/arduino/src/kernel/MessageService.cpp
@@ -0,0 +1,35 @@
+#include "Arduino.h"
+#include "MessageService.h"
+
+void MessageService::init(){
+    Serial.begin(9600);
+    msgRecived.reserve(256);
+    msgRecived = "";
+    msgAvailable = NULL;
+}
+
+void serialEvent(){
+    char c;
+    while (Serial.available()){
+        c = Serial.read(); 
+        if(c != '\n'){
+            MSGService.msgRecived += c;
+        }
+    }
+    MSGService.msgAvailable = true;
+}
+
+bool MessageService::isMessageAivailable(){
+    return msgAvailable;
+}
+
+String MessageService::reciveMessage(){
+    msgAvailable = false;
+    String app = msgRecived;
+    msgRecived = "";
+    return app;
+}
+
+void MessageService::sendMessage(String content){
+    Serial.println(content);
+}
