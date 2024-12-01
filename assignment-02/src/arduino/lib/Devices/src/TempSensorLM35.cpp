5ebf4d8bb6d3a2ad48f865172b093de53d6981a9
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2024-12-01 20:03:41 +0100

    deleted whitespaces

--- assignment-02/src/arduino/lib/Devices/src/TempSensorLM35.cpp
+++ assignment-02/src/arduino/lib/Devices/src/TempSensorLM35.cpp
@@ -8,7 +8,7 @@ TempSensorLM35::TempSensorLM35(int p) : pin(p){
   
 float TempSensorLM35::getTemperature(){
   int value = analogRead(pin);
-  float valueInVolt = value*VCC/1023;  
+  float valueInVolt = value*VCC/1023;
   float valueInCelsius = valueInVolt/0.01;
   return valueInCelsius;
 }
