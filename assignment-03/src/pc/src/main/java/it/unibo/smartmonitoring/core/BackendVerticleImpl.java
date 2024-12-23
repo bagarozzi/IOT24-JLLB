836564bf53f34d6c3191317b720564fe23f8b16f
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-12-23 11:54:11 +0100

    Added FSM's boilerplate

--- assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/core/BackendVerticleImpl.java
+++ assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/core/BackendVerticleImpl.java
@@ -7,14 +7,37 @@ import it.unibo.smartmonitoring.core.api.BackendVerticle;
 public class BackendVerticleImpl extends AbstractVerticle implements BackendVerticle {
 
     private State state;
+    private long stateTimestamp;
 
     @Override
     public void start() {
-        
+        setState(State.IDLE);
     }
 
     public void update() {
-        System.out.println("BackendVerticleImpl.update");
+        switch(state) {
+            case MANUAL:
+                break;
+            case NORMAL:
+                break;
+            case HOT:
+                break;
+            case TOO_HOT:
+                break;
+            case ALARM:
+                break;
+            case IDLE:
+                break;
+        }
+    }
+
+    private long elapsedTimeInState() {
+        return System.currentTimeMillis() - stateTimestamp;
+    }
+
+    private void setState(final State state) {
+        this.state = state;
+        stateTimestamp = System.currentTimeMillis();
     }
     
 }
