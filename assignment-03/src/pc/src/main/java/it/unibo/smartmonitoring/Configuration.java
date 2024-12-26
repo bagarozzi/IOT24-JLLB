fe1303431661d42b9ca17958a5e2e0c5f598a122
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-12-26 16:54:55 +0100

    Added event bus addresses

--- assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/Configuration.java
+++ assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/Configuration.java
@@ -2,8 +2,13 @@ package it.unibo.smartmonitoring;
 
 public final class Configuration {
     
+    /* MQTT Broker and client setup */
     public static final String MQTT_BROKER_ADDRESS = "broker.mqtt-dashboard.com";
-	public static final String ESP_TOPIC_NAME = "esiot-2024";
-    public static final String MQTT_CLIENT_EB_ADDR = "mqtt.smartmonitoring.com";
+	public static final String ESP_TOPIC_NAME = "temperature/smartwindow";
+
+    /* Event bus addresses: */
+    public static final String BACKEND_MQTT_EB_ADDR = "mqtt.backend.com";
+    public static final String BACKEND_HTTP_EB_ADDR = "http.backend.com";
+    public static final String BACKEND_ARDUINO_EB_ADDR = "arduino.backend.com";
 
 }
