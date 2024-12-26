268a43059d3e4128f6bfcf8014068edd74ab2aa3
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-12-26 16:40:47 +0100

    Added MQTT client implementation without the event bus part

--- assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/core/MQTTClientVerticle.java
+++ assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/core/MQTTClientVerticle.java
@@ -1,11 +1,45 @@
 package it.unibo.smartmonitoring.core;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.mqtt.MqttClient;
 
-/*
- * MQTT Agent
+import it.unibo.smartmonitoring.Configuration;
+
+/**
+ * An MQTT client that connects to the {@link MQTTServer} and
+ * subscribes to the topic where the ESP32 publishes the data.
+ * <br>
+ * With every new MQTT message, the client publishes it on the 
+ * event bus.
  */
 public class MQTTClientVerticle extends AbstractVerticle {
+	
+	public MQTTClientVerticle() { }
+
+	@Override
+	public void start() {		
+		MqttClient client = MqttClient.create(vertx);
+        EventBus eb = vertx.eventBus();
+		
+		client.connect(1883, Configuration.MQTT_BROKER_ADDRESS, c -> {
+
+			log("Connected to: " + Configuration.MQTT_BROKER_ADDRESS);
+			log("Subscribing to: " + Configuration.ESP_TOPIC_NAME);
+			client.publishHandler(message -> {
+                System.out.println("[MQTT AGENT]: Received message from ESP32");
+                //eb.publish(Configuration.MQTT_CLIENT_EB_ADDR, Buffer.buffer(message.payload().getBytes()));
+			})
+			.subscribe(Configuration.ESP_TOPIC_NAME, 2);		
+		});
+	}
+	
+
+	private void log(String msg) { 
+		System.out.println("[MQTT AGENT]: " + msg);
+	}
+
 
 
 }
