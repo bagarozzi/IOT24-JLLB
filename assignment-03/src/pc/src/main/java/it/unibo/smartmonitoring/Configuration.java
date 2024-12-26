69b2a39380b00b3281f769d5c72e430dfe5e52da
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-12-26 16:40:58 +0100

    Added configuration variables

---
+++ assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/Configuration.java
@@ -0,0 +1,9 @@
+package it.unibo.smartmonitoring;
+
+public final class Configuration {
+    
+    public static final String MQTT_BROKER_ADDRESS = "broker.mqtt-dashboard.com";
+	public static final String ESP_TOPIC_NAME = "esiot-2024";
+    public static final String MQTT_CLIENT_EB_ADDR = "mqtt.smartmonitoring.com";
+
+}
