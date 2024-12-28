64547d96f6af64e4f7ae991025e9ee0e70ce4adb
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-12-28 18:29:30 +0100

    added better logging

--- assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/core/MQTTClientVerticle.java
+++ assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/core/MQTTClientVerticle.java
@@ -25,10 +25,10 @@ public class MQTTClientVerticle extends AbstractVerticle {
 		
 		client.connect(1883, Configuration.MQTT_BROKER_ADDRESS, c -> {
 
-			log("Connected to: " + Configuration.MQTT_BROKER_ADDRESS);
-			log("Subscribing to: " + Configuration.ESP_TOPIC_NAME);
+			log("Connected to \"" + Configuration.MQTT_BROKER_ADDRESS + "\"");
+			log("Subscribing to: \"" + Configuration.ESP_TOPIC_NAME + "\"");
 			client.publishHandler(message -> {
-                System.out.println("[MQTT AGENT]: Received message from ESP32");
+                System.out.println("[MQTT-AGENT]: Received message from ESP32");
                 //eb.publish(Configuration.MQTT_CLIENT_EB_ADDR, Buffer.buffer(message.payload().getBytes()));
 			})
 			.subscribe(Configuration.ESP_TOPIC_NAME, 2);		
@@ -37,7 +37,7 @@ public class MQTTClientVerticle extends AbstractVerticle {
 	
 
 	private void log(String msg) { 
-		System.out.println("[MQTT AGENT]: " + msg);
+		System.out.println("[MQTT-AGENT]: " + msg);
 	}
 
 
