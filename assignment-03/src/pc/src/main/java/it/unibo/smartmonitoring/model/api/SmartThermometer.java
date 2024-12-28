571af92f7ea52bbf9e96dd2af30b6b5fda018be1
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2024-12-28 12:03:16 +0100

    Added thermometer interface

--- assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/model/api/SmartThermometer.java
+++ assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/model/api/SmartThermometer.java
@@ -2,4 +2,10 @@ package it.unibo.smartmonitoring.model.api;
 
 public interface SmartThermometer {
 
+    public void setFrequency(int frequency);
+
+    public void setTemperature(float temperature);
+
+    public void getTemperature();
+
 }
--- assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/model/impl/SmartThermometerImpl.java
+++ assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/model/impl/SmartThermometerImpl.java
@@ -3,5 +3,20 @@ package it.unibo.smartmonitoring.model.impl;
 import it.unibo.smartmonitoring.model.api.SmartThermometer;
 
 public class SmartThermometerImpl implements SmartThermometer {
+
+    private float temperature;
+    private int frequency;
+
+    @Override
+    public void setFrequency(int frequency) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setFrequency'");
+    }
+
+    @Override
+    public void getTemperature() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTemperature'");
+    }
     
 }
