2399d6bc10486f23205744d86bf1bb32dd7f7afc
juturbo <johndoe@example.com>
2024-12-30 16:22:26 +0100

    Started main task implementation

---
+++ assignment-03/src/arduino/src/tasks/OperatorMainTask.cpp
@@ -0,0 +1,53 @@
+#include "tasks/OperatorMainTask.h"
+#include <Arduino.h>
+#include "config.h"
+#include "kernel/Logger.h"
+
+#define DISCHARGE_TIME 5000
+#define OPENING_DISCHARGING_TIME 1000
+#define CLOSING_DISCHARGING_TIME 1000
+
+OperatorMainTask::OperatorMainTask(WindowController* pController, Dashboard* pDashboard): 
+    pController(pController), pDashboard(pDashboard){
+    setState(AUTOMATIC);
+}
+  
+void OperatorMainTask::tick(){
+    pDashboard->sync();
+    pDashboard->notifyNewState();
+    switch (state){    
+        case AUTOMATIC: {
+            if (this->checkAndSetJustEntered()){
+                Logger.log(F("[OM] automatic"));
+            }
+            
+            break;
+        }
+        case MANUAL: {
+            if (this->checkAndSetJustEntered()){
+                Logger.log(F("[OM] manual"));
+            }
+            
+            break;
+        }
+    }
+}
+
+
+void OperatorMainTask::setState(State s){
+    state = s;
+    stateTimestamp = millis();
+    justEntered = true;
+}
+
+long OperatorMainTask::elapsedTimeInState(){
+    return millis() - stateTimestamp;
+}
+
+bool OperatorMainTask::checkAndSetJustEntered(){
+    bool bak = justEntered;
+    if (justEntered){
+      justEntered = false;
+    }
+    return bak;
+}
--- assignment-03/src/arduino/src/tasks/OperatorMainTask.h
+++ assignment-03/src/arduino/src/tasks/OperatorMainTask.h
@@ -1,25 +1,25 @@
-#ifndef __OPERATOR_MAN_TASK__
-#define __OPERATOR_MAN_TASK__
+#ifndef __OPERATOR_MAIN_TASK__
+#define __OPERATOR_MAIN_TASK__
 
 #include "kernel/Task.h"
 #include "model/WindowController.h"
 #include "model/Dashboard.h"
 
-class OperatorManTask: public Task {
+class OperatorMainTask: public Task {
 
 public:
-  OperatorManTask(WindowController* pController, Dashboard* pDashboard); 
+  OperatorMainTask(WindowController* pController, Dashboard* pDashboard); 
   void tick();
 
 private:  
-  void setState(int state);
+  enum State{ AUTOMATIC, MANUAL } state;
+
+  void setState(State state);
   long elapsedTimeInState();
   void log(const String& msg);
   
   bool checkAndSetJustEntered();
   
-  enum { NORMAL, WAITING_FOR_DISCHARGING, OPENING_DISCHARGING, DISCHARGING, 
-         CLOSING_DISCHARGING, WAITING_FOR_MAINTENANCE } state;
   long stateTimestamp;
   bool justEntered;
 
