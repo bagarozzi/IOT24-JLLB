8ef669564e8b82aa76b6c86346dfd34ee6eafb73
Luca Venturini <luca.venturini14@studio.unibo.it>
2024-12-30 18:06:09 +0100

    implemented method setstate and timeInState

--- assignment-03/src/esp/src/Task.h
+++ assignment-03/src/esp/src/Task.h
@@ -1,38 +1,73 @@
 #ifndef __TASK__
 #define __TASK__
+#include <Arduino.h>
 
-class Task {
-  int myPeriod;
-  int timeElapsed;
-  bool active;
-  
+class Task
+{
+	int myPeriod;
+	int timeElapsed;
+	bool active;
+	int state;
+	bool justEntered;
+
 public:
-  virtual void init(int period){
-    myPeriod = period;  
-    timeElapsed = 0;
-    active = true;
-  }
+	virtual void init(int period)
+	{
+		myPeriod = period;
+		timeElapsed = 0;
+		active = true;
+	}
 
-  virtual void tick() = 0;
+	virtual void tick();
 
-  bool updateAndCheckTime(int basePeriod){
-    timeElapsed += basePeriod;
-    if (timeElapsed >= myPeriod){
-      timeElapsed = 0;
-      return true;
-    } else {
-      return false; 
-    }
-  }
+	bool updateAndCheckTime(int basePeriod)
+	{
+		timeElapsed += basePeriod;
+		if (timeElapsed >= myPeriod)
+		{
+			timeElapsed = 0;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
-  bool isActive(){
-    return active;
-  }
+	bool isActive()
+	{
+		return active;
+	}
 
-  void setActive(bool active){
-    this->active = active;
-  }
-  
+	void setActive(bool active)
+	{
+		this->active = active;
+	}
+
+	void setState(int state)
+	{
+		this->state = state;
+		justEntered = true;
+	}
+
+	int getState()
+	{
+		return state;
+	}
+
+	long elapsedTimeInState()
+	{
+		return millis() - timeElapsed;
+	}
+
+	void logOnce(const String &message)
+	{
+		Serial.println(message);
+		if (justEntered)
+		{
+			justEntered = false;
+		}
+	}
 };
 
 #endif
