8db0c36056c71a741f79a9d384172cc4134a104b
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-01-02 17:06:44 +0100

    method implementation

--- assignment-03/src/esp/src/SmartTemperatureSensor.cpp
+++ assignment-03/src/esp/src/SmartTemperatureSensor.cpp
@@ -1,1 +1,47 @@
+#include "SmartTemperatureSensor.h"
 
+
+#define GREEN_LED 1
+#define RED_LED 2
+#define TEMPERATURE_SENSOR 3
+
+SmartTemperatureSensor::SmartTemperatureSensor()
+{
+    greenLed = new Led(GREEN_LED);
+    redLed = new Led(RED_LED);
+    sensor = new TempSensorLM35(TEMPERATURE_SENSOR);
+}
+
+void SmartTemperatureSensor::setLedsToNormal()
+{
+    this->greenLed->switchOn();
+    this->redLed->switchOff();
+}
+void SmartTemperatureSensor::setLedsToError()
+{
+    this->greenLed->switchOff();
+    this->redLed->switchOn();
+}
+float SmartTemperatureSensor::getTemperature()
+{
+    return this->sensor->getTemperature();
+}
+
+void SmartTemperatureSensor::setStateAlarm()
+{
+    this->setState(ALARM);
+}
+
+void SmartTemperatureSensor::setState(int state)
+{
+    this->state = state;
+}
+
+bool SmartTemperatureSensor::isInAlarmState()
+{
+    return this->getState() == ALARM;
+}
+bool SmartTemperatureSensor::isInHotState()
+{
+    return this->getState() == HOT;
+}
--- assignment-03/src/esp/src/SmartTemperatureSensor.h
+++ assignment-03/src/esp/src/SmartTemperatureSensor.h
@@ -1,15 +1,33 @@
 #include "Task.h"
+#include "Led.h"
+#include "TempSensor.h"
+#include "TempSensorLM35.h"
 class SmartTemperatureSensor : public Task
 {
     private:
         enum State{
-        
             NORMAL,
-            FAST,
+            HOT,
+            TOO_HOT,
             ALARM,
         };
 
+        int state;
+        Led* greenLed;
+        Led* redLed;
+        TempSensor* sensor;
+
     public:
-        SmartTemperatureSensor(/* args */);
+        SmartTemperatureSensor();
+
+
+        void setStateAlarm();
+        void setState(int state);
+        void setLedsToNormal();
+        void setLedsToError();
+        float getTemperature();
+        bool isInAlarmState();
+        bool isInHotState();
+        
 };
 
