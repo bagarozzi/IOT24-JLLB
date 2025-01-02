58101aaa862ab6e367ed36199f42ca616e080a1c
Luca Venturini <luca.venturini14@studio.unibo.it>
2025-01-02 17:07:01 +0100

    method implementation

--- assignment-03/src/esp/src/TemperatureTask.h
+++ assignment-03/src/esp/src/TemperatureTask.h
@@ -1,17 +1,19 @@
 #include <Task.h>
 #include "MQTT_Agent.h"
+#include "TempSensorLM35.h"
+#include "SmartTemperatureSensor.h"
 class TemperatureTask : public Task
 {
 private:
+    SmartTemperatureSensor temperatureSensor;
     MQTT_agent agent;
-    //TempSensor sensor;
+
     enum State{
-        
             NORMAL,
             FAST,
             ALARM,
         };
 public:
-    TemperatureTask();
+    TemperatureTask(MQTT_agent agent, SmartTemperatureSensor sensor);
     void tick();
 };
