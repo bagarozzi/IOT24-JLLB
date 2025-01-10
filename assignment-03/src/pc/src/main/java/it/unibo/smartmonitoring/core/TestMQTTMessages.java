c6d3c3fa9de750f552e781a4e2311691bdc2091b
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2025-01-10 16:13:23 +0100

    Added tester verticle for MQTT messages

---
+++ assignment-03/src/pc/src/main/java/it/unibo/smartmonitoring/core/TestMQTTMessages.java
@@ -0,0 +1,63 @@
+package it.unibo.smartmonitoring.core;
+
+import io.netty.handler.codec.mqtt.MqttQoS;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.mqtt.MqttClient;
+
+import it.unibo.smartmonitoring.Configuration;
+
+/**
+ * An MQTT client that connects to the {@link MQTTServer} and
+ * subscribes to the topic where the ESP32 publishes the data.
+ * <br>
+ * With every new MQTT message, the client publishes it on the 
+ * event bus.
+ */
+public class TestMQTTMessages extends AbstractVerticle {
+	
+	public TestMQTTMessages() { }
+
+	@Override
+	public void start() {		
+		MqttClient client = MqttClient.create(vertx);
+        EventBus eb = vertx.eventBus();
+
+		log("connecting to \"" + Configuration.MQTT_BROKER_ADDRESS + "\"");
+		
+		client.connect(Configuration.MQTT_BROKER_PORT, Configuration.MQTT_BROKER_ADDRESS, c -> {
+
+			if(c.succeeded()) {
+				log("Connected to \"" + Configuration.MQTT_BROKER_ADDRESS + "\"");
+				log("Subscribing to: \"" + Configuration.ESP_FREQUENCY_TOPIC + "\"");
+				client.publishHandler(message -> {
+					System.out.println("[MQTT-AGENT]: Received message from ESP32");
+					System.out.println(message.payload().toString());
+				})
+				.subscribe(Configuration.ESP_FREQUENCY_TOPIC, 0, s -> {
+					if(s.succeeded()) {
+						log("Subscribed to: \"" + Configuration.ESP_FREQUENCY_TOPIC + "\"");
+					}
+					else if(s.failed()) {
+						log("Failed to subscribe to: \"" + Configuration.ESP_FREQUENCY_TOPIC + "\"");
+					}
+				});
+			}
+			else if(c.failed()) {
+				log("Failed to connect to \"" + Configuration.MQTT_BROKER_ADDRESS + "\"");
+				return;
+			}
+		});
+	}
+	
+
+	private void log(String msg) { 
+		System.out.println("[MQTT-AGENT]: " + msg);
+	}
+
+
+
+}
